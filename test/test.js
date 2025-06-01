e9277dad42c343be95a0ad660ed3d07c61d03b74
mogamoga <mogamoga.g1thu6@gmail.com>
2025-06-01 10:05:47 +0900

    ニャーニャーニャニャニャニャｱｰｲｷｿ

--- test/test.js
+++ test/test.js
@@ -1,22 +1,30 @@
 
-const a = document.querySelector("#a");
-const b = document.querySelector("#b");
+const elA = document.querySelector("#a");
+const elB = document.querySelector("#b");
+const elLink = document.querySelector("#link");
 
-const la = (points, event) => {
+const elClientX = document.querySelector("#client-x");
+const elClientY = document.querySelector("#client-y");
+function drawPoint(points) {
+    elClientX.innerText = points[0].clientX;
+    elClientY.innerText = points[0].clientY;
+}
+
+const lisA = (points, event) => {
+    drawPoint(points);
     alert("A");
 };
-const lb = (points, event) => {
+const lisB = (points, event) => {
     event.stopPropagation();
+    drawPoint(points);
     alert("B");
 };
-
-TapEvent.on(a, la);
-TapEvent.on(b, lb);
-
-// ---
-
-const link = document.querySelector("#link");
-TapEvent.on(link, (points, event) => {
+const lisLink = (points, event) => {
     event.preventDefault();
-    console.log("hogehoge");
-});
+    drawPoint(points);
+    alert("Link");
+};
+
+TapEvent.on(elA, lisA);
+TapEvent.on(elB, lisB);
+TapEvent.on(elLink, lisLink);
